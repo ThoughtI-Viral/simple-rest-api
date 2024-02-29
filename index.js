997f50892226390e99c52279c165ac6f67813381
ThoughtI-Viral <viral.gajera@thoughti.com>
2024-02-29 23:50:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+let express = require("express");
+let router = require("./app/router/router.js");
+
+let app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/", router);
+
+app.listen(8080, function () {
+    console.log("App listening on port 8080...");
+});
